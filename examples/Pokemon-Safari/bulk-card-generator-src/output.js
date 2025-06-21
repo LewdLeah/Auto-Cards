6f543edc88411b9f24d9d1d396aeea950585ed2e
LewdLeah <daisy.anais.197@gmail.com>
2025-06-20 22:26:25 -0400

    Update output.js

--- examples/Pokemon-Safari/bulk-card-generator-src/output.js
+++ examples/Pokemon-Safari/bulk-card-generator-src/output.js
@@ -1,1 +1,7 @@
-// wip
+// Your "Output" tab should look like this
+const modifier = (text) => {
+  text = AutoCards("output", text);
+  state.inhibit = false;
+  return {text};
+};
+modifier(text);
